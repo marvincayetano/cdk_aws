ad2378081c9c7f2760841c2d6850d4a2d213fb4b
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-12 20:09:49 -0500

    👍 Fix: folder struc

--- bin/cdk-workshop.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { WorkshopPipelineStack } from "../lib/pipeline-stack";
-
-const app = new cdk.App();
-new WorkshopPipelineStack(app, "CdkWorkshopPipelineStack");
